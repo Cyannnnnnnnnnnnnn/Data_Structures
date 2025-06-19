bdbdcd3044f449564a4c46d0da0d4d64dbf2dff4
Cyannnnnnnnnnnnnn <cybuitragop@unal.edu.co>
2025-06-18 23:37:42 -0500

    Proyecto creado

---
+++ src/main/java/com/mycompany/data_structures/Data_Structures.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.data_structures;
+
+/**
+ *
+ * @author Chan
+ */
+public class Data_Structures {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
