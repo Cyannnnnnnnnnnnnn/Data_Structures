0f3cc3bef27c88d92b8f08259d5132782cebfce0
Cyannnnnnnnnnnnnn <cybuitragop@unal.edu.co>
2025-06-21 09:23:16 -0500

    Botones agregados y clases panel creadas. Creacion de la clase 'Sequential cada'

---
+++ src/main/java/com/mycompany/data_structures/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.data_structures;
+
+/**
+ *
+ * @author Chan
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            Main_Form mainWindow = new Main_Form();  // Usa tu JFrame principal
+            mainWindow.setVisible(true);
+        });
+    }
+}
+
